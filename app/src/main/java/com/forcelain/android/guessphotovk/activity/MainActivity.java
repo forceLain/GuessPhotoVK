c0ea922866213e39029ca897cf08614a6c98928a
Artur Glyzin <forcelain@gmail.com>
2015-07-31 23:40:29 +0600

    get my friends with rx

--- app/src/main/java/com/forcelain/android/guessphotovk/activity/MainActivity.java
+++ app/src/main/java/com/forcelain/android/guessphotovk/activity/MainActivity.java
@@ -1,27 +1,95 @@
 package com.forcelain.android.guessphotovk.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.forcelain.android.guessphotovk.R;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKSdk;
 import com.vk.sdk.api.VKError;
 
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    @Bind(R.id.text_token) TextView tokenTextView;
+    @Bind(R.id.text_friends) TextView friendsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         VKSdk.initialize(this);
+    }
+
+    @OnClick(R.id.button_login)
+    void onLoginClicked(){
         VKSdk.login(this);
+    }
+
+    @OnClick(R.id.button_friends)
+    void onFirendsClicked(){
+        Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext(getFriends());
+                subscriber.onCompleted();
+            }
+        })
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(new Subscriber<String>() {
+                @Override
+                public void onCompleted() {
+
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    friendsTextView.setText(e.toString());
+                }
+
+                @Override
+                public void onNext(String s) {
+                    friendsTextView.setText(s);
+                }
+            });
+    }
+
+    private String getFriends() {
+        String result = null;
+        String accessToken = VKAccessToken.currentToken().accessToken;
+
+        String url = "https://api.vk.com/method/friends.get?access_token=" + accessToken;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            result = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
@@ -29,38 +97,15 @@ public class MainActivity extends ActionBarActivity {
         if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
             public void onResult(VKAccessToken res) {
-                // Пользователь успешно авторизовался
-                Log.d("@@@@", res.accessToken);
+                res.save();
+                tokenTextView.setText(res.accessToken);
             }
             @Override
             public void onError(VKError error) {
-                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
-                Log.d("@@@@", error.errorMessage);
+                tokenTextView.setText(error.errorMessage);
             }
         })) {
             super.onActivityResult(requestCode, resultCode, data);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
